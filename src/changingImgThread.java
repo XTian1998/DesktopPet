38fb5fd2cb7638dc17a1f41014257894c8bb57ed
Ztxttttz <47474335+Ztxttttz@users.noreply.github.com>
2020-05-08 00:25:33 +0800

    增加进食动作

--- src/changingImgThread.java
+++ src/changingImgThread.java
@@ -10,10 +10,22 @@ public class changingImgThread extends Thread {
         try{
             while(true){
                 if(actionType == 1){
+                    //拖拽动作
                     imgUrl = "_drag"+ i +".png";
                     TestBody.cgJLabelImg(TestBody.imgLabel, "kabi"+imgUrl);
                     i++;
                     if(i>2) i=1;
+                    Thread.sleep(200);
+                }
+                else if(actionType == 2){
+                    //进食动作
+                    imgUrl = "_eat" + i + ".png";
+                    TestBody.cgJLabelImg(TestBody.imgLabel, "kabi"+imgUrl);
+                    i++;
+                    if(i>6) {
+                        TestBody.cgJLabelImg(TestBody.imgLabel, "kabi_default.png");
+                        break;
+                    }
                     Thread.sleep(200);
                 }
             }
